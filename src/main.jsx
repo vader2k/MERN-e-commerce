df9eae0dc486b0298b7442351fb478ebd465b9f3
danie odukwe <kingdanny295@gmail.com>
2024-06-07 23:33:40 +0100

    @add/ created shop context that handles all products available in the store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ShopContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ShopContextProvider>
   </React.StrictMode>,
 )
