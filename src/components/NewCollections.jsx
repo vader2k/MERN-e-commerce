e0ac0a97a6080909ebba9cadae87f441f9877884
danie odukwe <kingdanny295@gmail.com>
2024-06-16 15:23:53 +0100

    @update/ added function to get new collection data from api

--- src/components/NewCollections.jsx
+++ src/components/NewCollections.jsx
@@ -1,14 +1,33 @@
-import new_collection from '../assets/new_collections'
+// import new_collection from '../assets/new_collections'
 import Items from './Items'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const NewCollections = () => {
+
+  const [newCollection, setNewCollection] = useState([])
+  
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const res = await axios.get("http://localhost:8000/server/v1/newCollection")
+        setNewCollection(res.data.data)
+        console.log(res.data.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[])
+
+
   return (
     <div className='flex flex-col items-center gap-[10px] mb-[100px] max-w-[1400px] mx-auto'>
         <h1 className='text-[#171717] text-[30px] font-semibold'>NEW COLLECTIONS</h1>
         <hr className='w-[150px] h-[4px] rounded-[10px] bg-[#252525]'/>
 
         <div className='mt-[50px] grid grid-cols-4 gap-[30px]'>
-            {new_collection.map((item, i) => (
+            {newCollection.map((item, i) => (
                 <Items 
                   key={i} 
                   id={item.id}
